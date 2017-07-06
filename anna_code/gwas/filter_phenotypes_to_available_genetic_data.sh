ec92f6d2aed4eff27f16bf42047ec0d10f6612c9
Anna Shcherbina <annashcherbina@gmail.com>
2017-07-06 11:44:38 -0700

    filter covariates by available genotypes

--- anna_code/gwas/filter_phenotypes_to_available_genetic_data.sh
+++ anna_code/gwas/filter_phenotypes_to_available_genetic_data.sh
@@ -1,9 +1,15 @@
+#Quantitative 
 #python filter_phenotypes_to_available_genetic_data.py --input_df /scratch/PI/euan/projects/ukbb/gwas/accelerometry_plink/v1/accelerometery_aggregate_phenotypes.continuous.txt \
 #       --subject_ids /scratch/PI/euan/projects/ukbb/data/genetic_data/subject_ids.txt \
 #       --outf /scratch/PI/euan/projects/ukbb/gwas/accelerometry_plink/v1/accelerometery_aggregate_phenotypes.continuous.filtered.txt
 
+#Categorical 
+#python filter_phenotypes_to_available_genetic_data.py --input_df /scratch/PI/euan/projects/ukbb/gwas/accelerometry_plink/v1/accelerometery_aggregate_phenotypes.categorical.txt \
+#       --subject_ids /scratch/PI/euan/projects/ukbb/data/genetic_data/subject_ids.txt \
+ #      --outf /scratch/PI/euan/projects/ukbb/gwas/accelerometry_plink/v1/accelerometery_aggregate_phenotypes.categorical.filtered.txt
 
-python filter_phenotypes_to_available_genetic_data.py --input_df /scratch/PI/euan/projects/ukbb/gwas/accelerometry_plink/v1/accelerometery_aggregate_phenotypes.categorical.txt \
+
+#Filter covariates
+python filter_phenotypes_to_available_genetic_data.py --input_df /scratch/PI/euan/projects/ukbb/gwas/accelerometry_plink/v1/accelerometery_covariates.txt \
        --subject_ids /scratch/PI/euan/projects/ukbb/data/genetic_data/subject_ids.txt \
-       --outf /scratch/PI/euan/projects/ukbb/gwas/accelerometry_plink/v1/accelerometery_aggregate_phenotypes.categorical.filtered.txt
-
+       --outf /scratch/PI/euan/projects/ukbb/gwas/accelerometry_plink/v1/accelerometery_covariates.filtered.txt
