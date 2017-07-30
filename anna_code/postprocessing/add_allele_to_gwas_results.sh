b3585b71e992523bc374a2ac6612fd5f978395ff
Anna Shcherbina <annashcherbina@gmail.com>
2017-07-30 10:13:45 -0700

    script to generate twas inputs by adding A1 & A2, excluding bad snp's, and selecting columns onof interest, i.e. SNP STAT A1 A

--- anna_code/postprocessing/add_allele_to_gwas_results.sh
+++ anna_code/postprocessing/add_allele_to_gwas_results.sh
@@ -1,2 +1,3 @@
 python add_allele_to_gwas_results.py --frequencies /oak/stanford/groups/euan/projects/ukbb/data/genetic_data/v2/EGAD00010001225/001/ukb_mfi_chr18_v2.txt \
-       --gwas_output activity_chrom18_gwas_hits.txt
+       --gwas_output activity_chrom18_gwas_hits.txt 
+
